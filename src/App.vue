ff4aebfb776b05659da50e63d78aef062f71ff81
Jack Weidman <jackweidman1@gmail.com>
2024-03-24 14:24:34 -0500

    working on Authentication

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <template>
-  <AddBook @add-book="addBook" class="centered"></AddBook>
-  
-  <div v-if="booksLoaded" class="books-container">
-    <BookObject
-      v-for="book in books"
-      :key="book.id"
-      :author="book.author"
-      :title="book.title"
-      :genre="book.genre"
-      :length="book.length"
-      :review="book.review"
-      @delete="deleteBook(book.id)"
-    ></BookObject>
-    
+  <div>
+    <base-card v-if="!user">
+      <input type="email" v-model="email" placeholder="Email">
+      <input type="password" v-model="password" placeholder="Password">
+      <button @click="signIn">Sign In</button>
+    </base-card>
+    <div v-else>
+      <button @click="signOut">Sign Out</button>
+
+      <AddBook @add-book="addBook" class="centered"></AddBook>
+
+      <div v-if="booksLoaded" class="books-container">
+        <BookObject
+          v-for="book in books"
+          :key="book.id"
+          :author="book.author"
+          :title="book.title"
+          :genre="book.genre"
+          :length="book.length"
+          :review="book.review"
+          @delete="deleteBook(book.id)"
+        ></BookObject>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import AddBook from './components/AddBook.vue';
 import BookObject from './components/BookObject.vue';
+
 export default {
   name: 'App',
   components: {
@@ -29,12 +41,41 @@ export default {
     return {
       books: [],
       booksLoaded: false,
+      email: '',
+      password: '',
+      user: null,
     };
   },
-  created(){
-    this.fetchBooks();
+  created() {
+    const auth = getAuth();
+    auth.onAuthStateChanged((user) => {
+      this.user = user;
+      if (user) {
+        this.fetchBooks();
+      }
+    });
   },
   methods: {
+    async signIn() {
+      try {
+        const auth = getAuth();
+        await signInWithEmailAndPassword(auth, this.email, this.password);
+        // Clear the form after successful sign-in
+        this.email = '';
+        this.password = '';
+      } catch (error) {
+        console.error('Error signing in:', error);
+        // Handle sign-in errors (if needed)
+      }
+    },
+    async signOut() {
+      try {
+        const auth = getAuth();
+        await signOut(auth);
+      } catch (error) {
+        console.error('Error signing out:', error);
+      }
+    },
     addBook(newBook) {
       this.books.push({
         id: this.books.length + 1,
@@ -44,8 +85,8 @@ export default {
         length: newBook.length,
       });
       this.books.sort((a, b) => {
-    return a.author.localeCompare(b.author);
-  });
+        return a.author.localeCompare(b.author);
+      });
     },
     deleteBook(id) {
       const index = this.books.findIndex((book) => book.id === id);
@@ -56,7 +97,9 @@ export default {
     async fetchBooks() {
       try {
         // Fetch data from Firebase
-        const response = await fetch('https://weidman-family-library-default-rtdb.firebaseio.com/books.json');
+        const response = await fetch(
+          'https://weidman-family-library-default-rtdb.firebaseio.com/books.json'
+        );
         const data = await response.json();
         // Update the local books array with the retrieved data
         if (data) {
@@ -67,7 +110,6 @@ export default {
             genre: data[key].genre,
             length: data[key].length,
           }));
-          
         }
       } catch (error) {
         console.error('Error fetching books:', error);
@@ -80,7 +122,6 @@ export default {
 </script>
 
 <style scoped>
-
 .centered {
   margin-bottom: 20px;
   text-align: center;
@@ -113,5 +154,4 @@ export default {
 body {
   background-color: rgb(75, 31, 13); /* Change to your desired background color */
 }
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
-
-
-
+import { initializeApp } from "firebase/app";
 import { createApp } from 'vue'
 import App from './App.vue'
 import BaseCard from './components/UI/BaseCard.vue'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD9mS2ktlfVmR-VU4puo9aMmpAFLTXQZxo",
+    authDomain: "weidman-family-library.firebaseapp.com",
+    databaseURL: "https://weidman-family-library-default-rtdb.firebaseio.com",
+    projectId: "weidman-family-library",
+    storageBucket: "weidman-family-library.appspot.com",
+    messagingSenderId: "166456432095",
+    appId: "1:166456432095:web:75ec4d0b775d59659ca06a"
+  };
+  
+  // Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+
 const app = createApp(App)
 
 app.component('base-card', BaseCard);
